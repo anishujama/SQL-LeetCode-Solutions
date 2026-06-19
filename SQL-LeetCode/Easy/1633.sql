1c4c455dcbc2892fe2d1b6330286377d3975bccd
Anishujama khan <anishkh9374@gmail.com>
2026-06-19 11:12:02 +0530

    Done

---
+++ SQL-LeetCode/Easy/1633.sql
@@ -0,0 +1,9 @@
+-- 1633. Percentage of Users Attended a Contest
+
+select contest_id,
+round(count(user_id) * 100.0 / (select count(*) from users),2)
+as percentage
+from Register
+group by contest_id
+order by percentage desc,
+contest_id asc;
