e90083f9ea87144f682cd93de910ac1649d96bd2
Anishujama khan <anishkh9374@gmail.com>
2026-06-22 14:21:12 +0530

    Add files via upload

---
+++ SQL-LeetCode/Easy/1211.sql
@@ -0,0 +1,6 @@
+-- 1211. Queries Quality and Percentage
+select query_name,
+round(avg(rating / position), 2 ) as quality,
+round(100 * avg(case when rating < 3 then 1 else 0 end),2) as poor_query_percentage
+from queries
+group by query_name;
